a7679a90e3902ab73d159f91bb73ee2b99492679
Beomki Yeo <beomki.yeo@gmail.com>
2024-12-19 01:37:58 -0800

    Add a clusterization tutorial

---
+++ tutorials/clusterization.cpp
@@ -0,0 +1,22 @@
+/** TRACCC library, part of the ACTS project (R&D line)
+ *
+ * (c) 2022-2024 CERN for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
+
+// Project include(s).
+#include "traccc/clusterization/clusterization_algorithm.hpp"
+#include "traccc/geometry/silicon_detector_description.hpp"
+#include "traccc/seeding/silicon_pixel_spacepoint_formation_algorithm.hpp"
+
+// VecMem include(s).
+#include <vecmem/memory/host_memory_resource.hpp>
+
+
+using namespace traccc;
+
+int main()
+{
+    return 1;
+}

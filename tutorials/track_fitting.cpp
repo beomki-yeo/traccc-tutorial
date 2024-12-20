f6af42b9e90e4a0ea8dd0df6ed5eaf74f4713f6a
Beomki Yeo <beomki.yeo@gmail.com>
2024-12-20 02:16:22 -0800

    Add tutorial file for track finding and fitting

---
+++ tutorials/track_fitting.cpp
@@ -0,0 +1,16 @@
+/** TRACCC tutorial for beginners
+ *
+ * (c) 2024 CERN for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
+
+// Project include(s).
+#include "traccc/definitions/common.hpp"
+
+using namespace traccc;
+
+int main()
+{
+    return 1;
+}

7dfbf571ec46e9761fe131f0f7bfd43ad77cb76d
Beomki Yeo <beomki.yeo@gmail.com>
2024-12-21 11:02:59 -0800

    Add the track finding tutorial file

---
+++ tutorials/track_finding.cpp
@@ -0,0 +1,32 @@
+/** TRACCC tutorial for beginners
+ *
+ * (c) 2024 CERN for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
+
+// traccc include(s).
+#include "traccc/definitions/common.hpp"
+#include "traccc/geometry/detector.hpp"
+#include "traccc/edm/measurement.hpp"
+#include "traccc/finding/combinatorial_kalman_filter_algorithm.hpp"
+
+// detray include(s).
+#include "detray/core/detector.hpp"
+#include "detray/detectors/bfield.hpp"
+#include "detray/navigation/navigator.hpp"
+#include "detray/propagator/propagator.hpp"
+#include "detray/propagator/rk_stepper.hpp"
+#include "detray/io/frontend/detector_reader.hpp"
+
+// VecMem include(s).
+#include <vecmem/memory/host_memory_resource.hpp>
+
+using namespace traccc;
+
+int main(){
+
+    return 1;
+
+}
+
